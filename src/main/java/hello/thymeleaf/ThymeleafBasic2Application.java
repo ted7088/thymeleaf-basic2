cd5c79e82bfdd3b0a5c1d4ed6a26db19b8fa1e6d
master <ted3131@diquest.com>
2024-12-05 14:12:25 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaf/ThymeleafBasic2Application.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasic2Application.class, args);
+	}
+
+}
